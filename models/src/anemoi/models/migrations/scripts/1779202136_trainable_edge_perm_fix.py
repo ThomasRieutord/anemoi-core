390d821bd4daff25b3b918c8edd5823a00110e6d
RIEUTORD Thomas <thomas.rieutord@meteo.fr>
2026-07-08 08:24:59 +0000

    chore: remove linter modifications

--- models/src/anemoi/models/migrations/scripts/1779202136_trainable_edge_perm_fix.py
+++ models/src/anemoi/models/migrations/scripts/1779202136_trainable_edge_perm_fix.py
@@ -1,4 +1,4 @@
-# (C) Copyright 2025-2026 Anemoi contributors.
+# (C) Copyright 2025 Anemoi contributors.
 #
 # This software is licensed under the terms of the Apache Licence Version 2.0
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
@@ -46,9 +46,7 @@ def migrate(ckpt: CkptType, model: torch.nn.Module | None = None) -> CkptType:
     """
 
     if model is None:
-        LOGGER.info(
-            "Skipping trainable edge permutation migration because no model was provided."
-        )
+        LOGGER.info("Skipping trainable edge permutation migration because no model was provided.")
         return ckpt
 
     state_dict = ckpt.get("state_dict", {})
@@ -56,9 +54,7 @@ def migrate(ckpt: CkptType, model: torch.nn.Module | None = None) -> CkptType:
     try:
         named_modules = list(model.named_modules())
     except AttributeError:
-        LOGGER.info(
-            "Model does not support named_modules(). Skipping trainable edge permutation migration."
-        )
+        LOGGER.info("Model does not support named_modules(). Skipping trainable edge permutation migration.")
         return ckpt
 
     for provider_path, graph_provider in named_modules:
@@ -66,12 +62,8 @@ def migrate(ckpt: CkptType, model: torch.nn.Module | None = None) -> CkptType:
             continue
 
         layout_key = graph_provider._TRAINABLE_LAYOUT_VERSION_KEY
-        trainable_key = (
-            f"{provider_path}.{_TRAINABLE_KEY}" if provider_path else _TRAINABLE_KEY
-        )
-        layout_version_key = (
-            f"{provider_path}.{layout_key}" if provider_path else layout_key
-        )
+        trainable_key = f"{provider_path}.{_TRAINABLE_KEY}" if provider_path else _TRAINABLE_KEY
+        layout_version_key = f"{provider_path}.{layout_key}" if provider_path else layout_key
         layout_version = state_dict.get(layout_version_key, 0)
         if isinstance(layout_version, torch.Tensor):
             layout_version = int(layout_version.item())
@@ -95,9 +87,7 @@ def migrate(ckpt: CkptType, model: torch.nn.Module | None = None) -> CkptType:
                 )
                 raise RuntimeError(msg)
 
-            state_dict[trainable_key] = trainable.index_select(
-                0, graph_provider.perm.to(device=trainable.device)
-            )
+            state_dict[trainable_key] = trainable.index_select(0, graph_provider.perm.to(device=trainable.device))
 
         state_dict[layout_version_key] = graph_provider.trainable_layout_version.clone()
 
